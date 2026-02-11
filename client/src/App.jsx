7971f6cfbae9f073b3c7e41ef2dfb2ce0d227bbd
rajam10 <rajabalan6300@email.com>
2026-02-12 01:51:12 +0530

    Fix DB error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
  import { Timer } from "./Timer";
 import { initEventLogger, logEvent, shutdownEventLogger } from "./eventLogger";
  
- const API_BASE = `${process.env.REACT_APP_API_URL}/api`;
+ const API_BASE = process.env.REACT_APP_API_URL + "/api";
  
  async function startAttempt(candidateId, durationSeconds) {
    const res = await fetch(`${API_BASE}/attempts/start`, {
