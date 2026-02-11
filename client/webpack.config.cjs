2d8bfd5e85a99618d3c575007635a0f403b1e196
rajam10 <rajabalan6300@email.com>
2026-02-11 23:47:56 +0530

    Fix Netlify deploy config

--- client/webpack.config.cjs
+++ client/webpack.config.cjs
@@ -3,11 +3,14 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.jsx",
+
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/"
+    publicPath: "/",
+    clean: true
   },
+
   module: {
     rules: [
       {
@@ -26,9 +29,16 @@ module.exports = {
       }
     ]
   },
+
   resolve: {
     extensions: [".js", ".jsx"]
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ],
 
   devServer: {
     static: {
@@ -36,12 +46,6 @@ module.exports = {
     },
     historyApiFallback: true,
     port: 3000,
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./public/index.html",
-      }),
-    ],
-
     proxy: [
       {
         context: ["/api"],
