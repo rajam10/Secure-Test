9ab7b30819ce77a3b20246d4ed872539ba4d7c6b
rajam10 <rajabalan6300@email.com>
2026-02-12 00:53:42 +0530

    Fix DB error

--- server/src/db.js
+++ server/src/db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   port: Number(process.env.MYSQLPORT || process.env.DB_PORT),
   user: process.env.MYSQLUSER || process.env.DB_USER ,
   password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD ,
-  database: process.env.MYSQL_DATABASE || process.env.DB_NAME ,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME ,
   connectionLimit: 10
 });
  
